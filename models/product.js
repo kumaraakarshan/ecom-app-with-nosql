c223b891fd405cb4574930211459768fc36a0429
kumaraakarshan <aakarshan0005@gmail.com>
2023-09-05 12:33:59 +0530

    creating products

---
+++ models/product.js
@@ -0,0 +1,23 @@
+const getDb = require('../util/database').getDb;
+
+class Product{
+  constructor(title,price,description,imageURL){
+      this.title = title;
+      this.price = price;
+      this.description = description;
+      this.imageURL = imageURL;
+  }
+  save(){
+      const db = getDb();
+      return db.collection('products')
+      .insertOne(this)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+}
+
+module.exports = Product;

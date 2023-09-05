aecbf3773c5683f26e726b672833d9caf1bfd429
kumaraakarshan <aakarshan0005@gmail.com>
2023-09-05 13:18:10 +0530

    crud with mongoDB

--- models/product.js
+++ models/product.js
@@ -1,14 +1,23 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product{
-  constructor(title,price,description,imageURL){
+  constructor(title,price,description,imageURL,id){
       this.title = title;
       this.price = price;
       this.description = description;
       this.imageURL = imageURL;
+      this._id = id ? new mongodb.ObjectId(id) : null; 
   }
   save(){
       const db = getDb();
+      let dbOp;
+      if(this._id){
+        //update the product
+        dbOp = db.collection('products').updateOne({_id: this._id}, {$set: this})
+      } else {
+        dbOp = db.collection('products').insertOne(this)
+      }
       return db.collection('products')
       .insertOne(this)
       .then(result => {
@@ -18,6 +27,47 @@ class Product{
         console.log(err)
       })
   }
+
+  static fetchAll() {
+    const db = getDb();
+    return db.collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+          console.log(products)
+          return products;
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+  static findById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+      .find({_id: new mongodb.ObjectId(prodId)})
+      .next()
+      .then(products => {
+          console.log(products)
+          return products;
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+      .deleteOne({_id: new mongodb.ObjectId(prodId)})
+      .then(products => {
+          console.log('Deleted')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 }
+
+
 
 module.exports = Product;
